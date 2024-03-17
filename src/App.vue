dbd9ce752bcc30550c2d71677c2a4e888369f3d6
SilantevDenis <d.silantjev2012@gmail.com>
2024-03-17 17:08:50 +0300

    start develop with vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <el-container >
+    <el-header>
+      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+        <el-tab-pane label="User" name="first">User</el-tab-pane>
+        <el-tab-pane label="Config" name="second">Config</el-tab-pane>
+        <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+        <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+      </el-tabs>
+    </el-header>
+    <el-main>
+      sd
+    </el-main>
+  </el-container>
+</template>
+
+
